e76be9b19b01a0d2261136e4a94fe341c6a6b343
ayush khanduri <ayush@metadesignsolutions.co.uk>
2019-08-11 22:51:59 +0530

    css working

--- src/app.tsx
+++ src/app.tsx
@@ -4,14 +4,13 @@ import { Button } from 'antd';
 
 
 import AppRouter from './AppRouter';
-import './app.css';
 
-ReactDOM.render(<Button type="primary"> Primary</Button>,document.getElementById('app'));
+ReactDOM.render(<AppRouter/>,document.getElementById('app'));
 
 if(module.hot){
     module.hot.accept('./AppRouter.tsx',()=>{
         console.log("Asdasd");
         const AppRouter = require('./AppRouter.tsx').default;
-        ReactDOM.render(<Button type="primary"> Primary</Button>,document.getElementById('app'));
+        ReactDOM.render(<AppRouter/>,document.getElementById('app'));
     });
 }
