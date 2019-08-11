cd4a1b45c1ef4f65c3af0d582d39830542c5c8f4
ayush <ayushkhanduri@gmail.com>
2019-08-11 19:50:46 +0530

    latest packages update

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Button } from 'antd';
+
 
 import AppRouter from './AppRouter';
-// import 'antd/dist/antd.css';
+import './app.css';
 
-ReactDOM.render(<AppRouter/>,document.getElementById('app'));
+ReactDOM.render(<Button type="primary"> Primary</Button>,document.getElementById('app'));
 
 if(module.hot){
     module.hot.accept('./AppRouter.tsx',()=>{
         console.log("Asdasd");
         const AppRouter = require('./AppRouter.tsx').default;
-        ReactDOM.render(<AppRouter/>,document.getElementById('app'));
+        ReactDOM.render(<Button type="primary"> Primary</Button>,document.getElementById('app'));
     });
 }
